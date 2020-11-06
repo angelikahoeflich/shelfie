21cc4cfa76ff30778bee45e0d8cee86a6d7efd8f
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2020-11-06 10:00:38 -0800

    added form actions

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,10 +1,60 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class Form extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      imageURL: '',
+      productName: '',
+      price: 0
+    }
+
+    this.handleCancel = this.handleCancel.bind(this);
+    this.handleImageChange = this.handleImageChange.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handlePriceChange = this.handlePriceChange.bind(this);
+   
+    console.log('wtf')
+  }
+
+  handleCancel(click){
+    this.setState({
+      imageURL: '',
+      productName: '',
+      price: 0
+    })
+  }
+
+  handleImageChange(event){
+    this.setState({
+      imageURL: event.target.value
+    })
+  }
+  handleNameChange(event){
+    this.setState({
+      productName: event.target.value
+    })
+  }
+  handlePriceChange(event){
+    this.setState({
+      price: event.target.value
+    })
+  }
+
+
   render() {
+    const {imageURL, productName, price} = this.state;
     return (
       <div>
-        <h1>Form</h1>
+        <h2>Image URL: </h2>
+        <input value={imageURL} onChange={this.handleImageChange}/>
+        <h2>Product Name:</h2>
+        <input value={productName} onChange={this.handleNameChange} />
+        <h2>Price:</h2>
+        <input value={price} onChange={this.handlePriceChange}/>
+        <button onClick={this.handleCancel}>Cancel</button>
+        <button>Add</button>
       </div>
     )
   }
