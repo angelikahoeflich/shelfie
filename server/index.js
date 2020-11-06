a8b1e06f8639312ee41935dbaa01993db1847777
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2020-11-06 09:08:27 -0800

    connected to db

--- server/index.js
+++ server/index.js
@@ -3,8 +3,19 @@ const express = require('express');
 const massive = require('massive');
 const app = express();
 app.use(express.json());
+const {SERVER_PORT, CONNECTION_STRING} = process.env;
+
+massive({
+  connectionString: CONNECTION_STRING,
+
+  ssl: {
+    rejectUnauthorized:false
+  }
+}).then((db) => {
+  app.set('db', db);
+});
 
 //endpoints
 
 
-app.listen(4000, console.log('listening on da port'));
+app.listen(SERVER_PORT, console.log('listening on da port'));
